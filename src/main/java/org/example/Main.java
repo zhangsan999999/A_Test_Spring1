70bcb14d061d7a82169acc2bf858910cd6294941
“fangfang” <1736525203@qq.com>
2023-08-27 18:41:56 +0800

    添加了test方法

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println(111);
+        test();
+    }
+    public static void test(){
+        System.out.println(9999999);
     }
 }
