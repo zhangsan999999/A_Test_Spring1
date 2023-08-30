270bf94b51588075e9146e219274847500aa5838
“fangfang” <1736525203@qq.com>
2023-08-30 21:18:54 +0800

    添加了test方法jj777111

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.println(111);
         System.out.println("i love you");
         System.out.println(18811);
-        System.out.println(9999)
+        System.out.println(9999);
+        System.out.println(1111111111);
     }
 }
