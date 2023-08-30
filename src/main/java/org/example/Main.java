ebcc2cbd5b1fcbcf2f9c7e9983ec61bd07599eaa
“fangfang” <1736525203@qq.com>
2023-08-30 18:01:05 +0800

    添加了test方法jj777

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println(111);
         System.out.println("i love you");
+        System.out.println(18811);
     }
 }
