bc62fb4e7d0b52a35c7c2e445690afb1a3ac7dae
“fangfang” <1736525203@qq.com>
2023-08-30 17:57:43 +0800

    添加了test方法jj

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println(111);
+        System.out.println("i love you");
     }
 }
